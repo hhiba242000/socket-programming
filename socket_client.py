a0d21b137a6ddb215546bf6eb41ab42a449714d8
Hayam Hiba <hayam.hiba.ext@brightskiesinc.com>
2022-12-27 00:03:58 +0200

    Final commit

--- socket_client.py
+++ socket_client.py
@@ -7,9 +7,6 @@ from typing import List
 host = "127.0.0.1"
 port = 8027  # take it as argument from command line
 
-
-# s.send(b'POST /session HTTP/1.1\r\nHost: 127.0.0.1:9515\r\nContent-Type: application/json\r\nContent-Length: 47\r\n\r\n{"capabilities": {}, "desiredCapabilities": {}}')
-
 def prepare_packet(port, host, data):
     i = len(data)
     print(data + " &&&&&&&" + str(i))
@@ -24,36 +21,39 @@ def extract_data(data):
     return msg
 
 def client_program():
-        os.environ['NO_PROXY'] = '127.0.0.1'
-
-        client_socket = socket.socket(socket.AF_INET,
-                                      socket.SOCK_STREAM)  # SOCK_STREAM for TCP sockets and SOCK_DGRAM for UDP sockets
+        client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  # SOCK_STREAM for TCP sockets and SOCK_DGRAM for UDP sockets
         client_socket.connect((host, port))
 
-        jsonResult = {}
-        jsonResult['equations'] = []
         while True:
-            message = input(" -> ")
-            if message == '':
+            jsonResult = {}
+            jsonResult['equations'] = []
+            while True:
+                message = input(" -> ")
+                if message == '':
+                    break
+                else:
+                    jsonResult['equations'].append(message)
+
+
+            jsonResult = json.dumps(jsonResult)
+            jsonResult = prepare_packet(port, host,jsonResult)
+
+            client_socket.send(bytes(jsonResult.encode()))
+            print("waiting to receive in client\n")
+            data = client_socket.recv(1024).decode()
+            print("after receive in client\n")
+            print(data)
+            if not data:
                 break
-            else:
-                jsonResult['equations'].append(message)
+            data = extract_data(data)
+            print("after extraction")
+            if data == "bye":
+                print("Client is notified you left")
+                break
 
-        jsonResult = json.dumps(jsonResult)
-        jsonResult = prepare_packet(port, host,jsonResult)
+            print('Received from server: ' + str(data))
 
-        client_socket.send(bytes(jsonResult.encode()))  # send message
-        # client_socket.settimeout(10)
-        print("waiting to receive in client\n")
-        data = client_socket.recv(1024).decode()
-        print("after receive in client\n")# receive response
-        data = extract_data(data)
-        if data == "bye":
-            print("Client is notified you left")
-
-        print('Received from server: ' + str(data))  # show in terminal
-
-        # client_socket.close()  # close the connection
+        client_socket.close()  # close the connection
 
 
 if __name__ == '__main__':
--- socket_server.py
+++ socket_server.py
@@ -9,9 +9,6 @@ from typing import List
 
 
 
-host = "127.0.0.1"  # Standard loopback interface address (localhost)
-port = 8027
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print ( f'Hi, {name}' )  # Press Ctrl+F8 to toggle the breakpoint.
@@ -84,61 +81,89 @@ def signal_handler(signal, frame):
     print('\nYou pressed Ctrl+C, keyboardInterrupt detected,Server is exiting!')
     sys.exit(0)
 
+def calculate_equation(s):
+    inp = str(s)
+    if inp.__contains__("sin"):
+        inp = inp.replace("sin(", str(math.sin(float(inp[4:inp.index(")")]))))
+        tmp = inp[:inp.index(".") + 1]
+        inp = inp[inp.index("."):].replace(".", "")
+        inp = inp.replace(")", "")
+        inp = tmp + inp
+
+    elif inp.__contains__("exp"):
+        inp = inp.replace("exp(", str(math.exp(float(inp[4:inp.index(")")]))))
+        tmp = inp[:inp.index(".") + 1]
+        inp = inp[inp.index("."):].replace(".", "")
+        inp = inp.replace(")", "")
+        inp = tmp + inp
+
+    out = infixToPostfix(inp)
+    num = postfixEvaluator(out)
+    return num
 
 def server_program():
-    signal.signal(signal.SIGINT, signal_handler)
+    host = "127.0.0.1"  # Standard loopback interface address (localhost)
+    port = 8027
+    if len(sys.argv) == 3:
+        host = str(sys.argv[1])
+        port = int(sys.argv[2])
 
+    signal.signal(signal.SIGINT, signal_handler)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind ( (host, port) )
-    server_socket.listen()
     while True:
-        conn, address = server_socket.accept()
-        with conn:
-            print("Connection from: " + str(address))
-            print("before receive\n")
-            data = conn.recv(1024).decode()
-            print("after receive "+str(data)+"\n")
-            split_post = data.split("\r\n")
-            print(split_post)
-            msg = split_post[4]
-            print("message "+str(msg)+"\n")
-            dict_of_eq = json.loads(str(msg))
-            list_of_eq = dict_of_eq['equations']
-            print(list_of_eq)
-            results = {}
-            results['result'] = []
-            res=[]
-            for s in list_of_eq:
-                inp = str(s)
-                if inp.__contains__ ( "sin" ):
-                    inp = inp.replace ( "sin(", str ( math.sin ( float ( inp[4:inp.index(")")] ) ) ) )
-                    tmp = inp[:inp.index(".")+1]
-                    inp = inp[inp.index("."):].replace(".", "")
-                    inp = inp.replace ( ")", "" )
-                    inp = tmp + inp
+        server_socket.listen()
+        while True:
+            conn, address = server_socket.accept()
+            with conn:
+                print("Connection from: " + str(address))
+                print("before receive\n")
+                while True:
+                    data = conn.recv(1024).decode()
+                    print('dataaaa'+str(data))
+                    if not data:
+                        data = f'HTTP/1.0 500 \r\n'
+                        print(data)
+                        data = (str(data)).encode()
+                        conn.send(data)
+                        break
+                    print("after receive "+str(data)+"\n")
+                    split_post = data.split("\r\n")
+                    print(split_post)
+                    msg = split_post[4]
+                    print("message "+str(msg)+"\n")
+                    dict_of_eq = json.loads(str(msg))
+                    list_of_eq = dict_of_eq['equations']
+                    print(list_of_eq)
+                    results = {}
+                    results['qid'] = 1234
+                    results['query'] = []
+                    res=[]
+                    i=1
+                    for s in list_of_eq:
+                        temp = {}
+                        temp['id'] = str(i)
+                        temp['requestedLine'] = str(s)
 
-                elif inp.__contains__ ( "exp" ):
-                    inp = inp.replace ( "exp(", str ( math.exp ( float ( inp[4:inp.index(")")] ) ) ) )
-                    tmp = inp[:inp.index(".")+1]
-                    inp = inp[inp.index("."):].replace(".", "")
-                    inp = inp.replace ( ")", "" )
-                    inp = tmp + inp
+                        num = calculate_equation(s)
+                        if type(num) == float:
+                            temp['result'] = str(num)
+                            #res.append(str(num))
+                        else:
+                            temp['result'] = 'error'
+                            #res.append('error')
 
-                out = infixToPostfix(inp)
-                num = postfixEvaluator(out)
-                if type(num) == float:
-                    res.append(str(num))
-                else:
-                    res.append('error')
+                        res.append(temp)
+                        i=i+1
+                    results['query'] = res
+                    print("OUT OF FOR LOOP\n")
+                    print(list_of_eq)
+                    data = f'HTTP/1.0 200 OK\r\n{results}\r\n'
+                    print(data)
+                    data = (str(data)).encode()
+                    conn.send(data)
 
-                results['result'].append(res)
-                print("dictionary")
-            print(list_of_eq)
-            data = f'HTTP/1.0 200 OK\r\n{results}\r\n'
-            print(data)
-            data = (str(data)).encode()
-            conn.send(data)
 
         
         
